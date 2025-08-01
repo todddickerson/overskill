7c9a6f8be389622ee6af3d1dee664e1a76718b98
Todd Dickerson <todd@spontent.com>
2025-08-01 11:01:27 -0400

    Fix DeploymentChannel authorization error
    - Fixed undefined method 'team' error for User in DeploymentChannel
    - Updated to check if user has access through any of their teams
    - Properly handles BulletTrain's multi-tenancy where users can belong to multiple teams

--- app/channels/deployment_channel.rb
+++ app/channels/deployment_channel.rb
@@ -1,7 +1,14 @@
 class DeploymentChannel < ApplicationCable::Channel
   def subscribed
-    app = current_user.team.apps.find(params[:app_id])
-    stream_from "app_#{app.id}_deployment"
+    # Find the app and verify the user has access through any of their teams
+    app = App.find(params[:app_id])
+    
+    # Check if the user has access to this app through any of their teams
+    if current_user.teams.include?(app.team)
+      stream_from "app_#{app.id}_deployment"
+    else
+      reject
+    end
   rescue ActiveRecord::RecordNotFound
     reject
   end
