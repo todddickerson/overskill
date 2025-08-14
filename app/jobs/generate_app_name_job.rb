752c5b2f3e1028a46f78dcf5969c07faed979c57
Todd Dickerson <todd@spontent.com>
2025-08-14 09:10:58 -0400

    fix app name job and prompt missing os- prefix

--- app/jobs/generate_app_name_job.rb
+++ app/jobs/generate_app_name_job.rb
@@ -8,8 +8,8 @@ class GenerateAppNameJob < ApplicationJob
     app = App.find(app_id)
     
     # Skip if app already has a good name (not default/generic)
-    if has_good_name?(app)
-      Rails.logger.info "[AppName] Skipping name generation for app #{app.id} - already has good name: '#{app.name}'"
+    if app.name_generated_at.present?
+      Rails.logger.info "[AppName] Skipping name generation for app #{app.id} - already generated name: '#{app.name}'"
       return
     end
 
@@ -35,45 +35,6 @@ class GenerateAppNameJob < ApplicationJob
   end
 
   private
-
-  def has_good_name?(app)
-    return false if app.name.blank?
-    
-    # Check if name looks auto-generated or generic
-    generic_patterns = [
-      /^App \d+$/i,                    # "App 123"
-      /^New App$/i,                    # "New App"
-      /^Untitled App$/i,               # "Untitled App"
-      /^My App$/i,                     # "My App"
-      /^Test App$/i,                   # "Test App"
-      /^Draft \d+$/i,                  # "Draft 123"
-      /^Application$/i,                # "Application"
-      /^\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$/  # UUID-like
-    ]
-    
-    # If name matches any generic pattern, it needs a better name
-    return false if generic_patterns.any? { |pattern| app.name.match?(pattern) }
-    
-    # If name was recently generated, don't regenerate
-    return true if app.name_generated_at && app.name_generated_at > 1.day.ago
-    
-    # If name is descriptive (more than 2 words or contains meaningful content), keep it
-    return true if app.name.split.length > 2
-    
-    # If name seems intentionally set by user (contains creative elements), keep it
-    creative_indicators = [
-      /[A-Z][a-z]+[A-Z]/,  # CamelCase like "TaskFlow"
-      /\w+ly$/,            # Ends with "ly" like "Quickly"
-      /\w+er$/,            # Ends with "er" like "Tracker"
-      /\w+hub$/i,          # Ends with "hub" like "TaskHub"
-      /\w+pro$/i,          # Ends with "pro" like "BudgetPro"
-    ]
-    
-    return true if creative_indicators.any? { |pattern| app.name.match?(pattern) }
-    
-    # Default to generating a new name for short, generic names
-    false
-  end
 
   def broadcast_navigation_update(app)
     # Broadcast to all users who might be viewing this app's editor
