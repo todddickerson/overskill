e9a09918beb036a54d6360202df6de7f29141600
Todd Dickerson <todd@spontent.com>
2025-08-22 12:51:28 -0400

    Remove unused CloudflarePreviewServiceR2Patch
    - V5 deployment flow uses GitHub + Workers for Platforms, not CloudflarePreviewService
    - Removing obsolete R2 patch that was causing Zeitwerk autoloading CI failures
    - Fixes AI service tests by eliminating the problematic patch completely

--- config/initializers/cloudflare_r2_patch.rb
+++
@@ -1,6 +0,0 @@
-# Apply CloudflarePreviewService R2 patch
-Rails.application.config.after_initialize do
-  if defined?(Patches::CloudflarePreviewServiceR2Patch)
-    Patches::CloudflarePreviewServiceR2Patch.apply!
-  end
-end
